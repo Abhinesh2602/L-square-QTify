8e4bbe28b038cbd894cbd2be875e60a4d7b4f038
Abhinesh <your.email@example.com>
2024-04-26 19:45:36 +0530

    created hero section

---
+++ qtify/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import headphone from "./../../assets/vibrating-headphone.png";
+
+function Hero() {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.title}>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes </h1>
+      </div>
+      <div>
+        <img src={headphone} alt="headphone" className={styles.heroImage} />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
